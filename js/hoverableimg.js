45524450706880d13ce6bf02f37b8bd790fe42d7
Whitney Harden <wtharden320@gmail.com>
2021-04-01 12:27:37 -0400

    made hover available for multiple divs

--- js/hoverableimg.js
+++ js/hoverableimg.js
@@ -1,22 +1,35 @@
-const hoverableDiv = document.querySelector('.hoverableDiv');
-const hoverabletext = document.querySelector('.hoverabletext');
+const hoverableDivList = document.querySelectorAll('.hoverableDiv');
+// const hoverabletext = document.querySelector('.hoverabletext');
+
+console.log('hoverableDiv ', hoverableDivList);
 
 let clicked = false;
 
-hoverableDiv.addEventListener('mouseover', () => {
-  // console.log(e);
-  //reduce the opacity on the picture and have text pop up and be visible to read
-  //display hoverabletext add class
-  hoverabletext.classList.remove('d-none');
-
-  hoverableDiv.addEventListener('mouseout', () => {
-    hoverabletext.classList.add('d-none');
+hoverableDivList.forEach(div => {
+  console.log('div', div);
+  const hoverabletext = div.querySelector('.hoverabletext');
+  div.addEventListener('mouseover', () => {
+    hoverabletext.classList.remove('d-none');
+    div.addEventListener('mouseout', () => {
+      hoverabletext.classList.add('d-none');
+    });
   });
 });
 
-hoverableDiv.addEventListener('click', e => {
-  e.preventDefault();
-  clicked = !clicked;
-  console.log(clicked);
-  // allow the text to stay even if you
-});
+// hoverableDiv.addEventListener('mouseover', () => {
+//   // console.log(e);
+//   //reduce the opacity on the picture and have text pop up and be visible to read
+//   //display hoverabletext add class
+//   hoverabletext.classList.remove('d-none');
+
+//   hoverableDiv.addEventListener('mouseout', () => {
+//     hoverabletext.classList.add('d-none');
+//   });
+// });
+
+// hoverableDiv.addEventListener('click', e => {
+//   e.preventDefault();
+//   clicked = !clicked;
+//   console.log(clicked);
+//   // allow the text to stay even if you
+// });
